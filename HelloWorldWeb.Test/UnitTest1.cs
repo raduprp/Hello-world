db31a63186a88e16a7c19e95240e1fb0a4c44726
parpa <parpa@DESKTOP-1JUTT8T>
2021-08-05 09:51:50 +0300

    UnitTest for AddTeamMember

---
+++ HelloWorldWeb.Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+using HelloWorldWebApp.Services;
+using System;
+using Xunit;
+
+namespace HelloWorldWeb.Test
+{
+    public class TeamServiceTest
+    {
+        [Fact]
+        public void AddTeamMemberToTheTeam()
+        {
+            // Assume
+
+            ITeamService teamService = new TeamService();
+
+            // Act
+
+            teamService.AddTeamMember("George");
+
+            // Assert
+            Assert.Equal(6, teamService.GetTeamInfo().TeamMembers.Count);
+            
+        }
+    }
+}
