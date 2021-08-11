24a0f173be16324ac450f15043b9b2622de2436a
parpa <parpa@DESKTOP-1JUTT8T>
2021-08-11 12:22:31 +0300

    Tested Added and working

--- HelloWorldWeb.Test/UnitTest1.cs
+++ HelloWorldWeb.Test/UnitTest1.cs
@@ -20,5 +20,49 @@ namespace HelloWorldWeb.Test
             Assert.Equal(6, teamService.GetTeamInfo().TeamMembers.Count);
             
         }
+
+
+        [Fact]
+        public void DeleteTeamMemberFromTheTeam()
+        {
+            //Assume
+            ITeamService teamService = new TeamService();
+            //Act
+            teamService.DeleteTeamMember(3);
+            //Assert
+            Assert.Equal(5, teamService.GetTeamInfo().TeamMembers.Count);
+        }
+
+        [Fact]
+        public void EditTeamMemberInTheTeam()
+        {
+            //Assume
+            ITeamService teamService = new TeamService();
+            var targetTeamMember = teamService.GetTeamInfo().TeamMembers[0];
+            var memberId = targetTeamMember.Id;
+            //Act
+            teamService.EditTeamMember(memberId, "NewName");
+            //Assert
+            Assert.Equal("NewName", teamService.GetTeamMemberById(memberId).Name);
+        }
+
+        [Fact]
+        public void CheckIdProblem()
+        {
+            //Asume
+            ITeamService teamService = new TeamService();
+            var memberToBeDeleted = teamService.GetTeamInfo().TeamMembers[teamService.GetTeamInfo().TeamMembers.Count - 2];
+            var newMemberName = "Boris";
+            //Act
+            teamService.DeleteTeamMember(memberToBeDeleted.Id);
+            var id = teamService.AddTeamMember(newMemberName);
+            teamService.DeleteTeamMember(id);
+            //Assert
+            var member = teamService.GetTeamInfo().TeamMembers.Find(element => element.Name == "Boris");
+            Assert.Null(member);
+        }
+
     }
+
+
 }
