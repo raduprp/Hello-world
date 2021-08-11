7b6b2475804b56cb0c6ebe572ba616b208069981
parpa <parpa@DESKTOP-1JUTT8T>
2021-08-11 22:10:40 +0300

    Test driven dev - add test for getting age

---
+++ HelloWorldWeb.Test/TeamServiceTests.cs
@@ -0,0 +1,103 @@
+using HelloWorldWeb.Models;
+using HelloWorldWeb.Services;
+using System;
+using Xunit;
+
+namespace HelloWorldWeb.Test
+{
+    public class TeamServiceTests
+    {
+        [Fact]
+        public void AddTeamMemberToTheTeam()
+        {
+            // Assume
+
+            ITeamService teamService = new TeamService();
+
+            // Act
+
+            teamService.AddTeamMember("George");
+
+            // Assert
+            Assert.Equal(6, teamService.GetTeamInfo().TeamMembers.Count);
+            
+        }
+
+
+        [Fact]
+        public void DeleteTeamMemberFromTheTeam()
+        {
+            //Assume
+            ITeamService teamService = new TeamService();
+            var targetTeamMember = teamService.GetTeamInfo().TeamMembers[1];
+            int targetId = targetTeamMember.Id;
+            //Act
+            teamService.DeleteTeamMember(targetId);
+            //Assert
+            Assert.Equal(4, teamService.GetTeamInfo().TeamMembers.Count);
+        }
+
+        [Fact]
+        public void EditTeamMemberInTheTeam()
+        {
+            //Assume
+            ITeamService teamService = new TeamService();
+            var targetTeamMember = teamService.GetTeamInfo().TeamMembers[0];
+            var memberId = targetTeamMember.Id;
+            //Act
+            teamService.EditTeamMember(memberId, "NewName");
+            //Assert
+            Assert.Equal("NewName", teamService.GetTeamMemberById(memberId).Name);
+        }
+
+        [Fact]
+        public void CheckIdProblem()
+        {
+            //Asume
+            ITeamService teamService = new TeamService();
+            var memberToBeDeleted = teamService.GetTeamInfo().TeamMembers[teamService.GetTeamInfo().TeamMembers.Count - 2];
+            var newMemberName = "Boris";
+            //Act
+            teamService.DeleteTeamMember(memberToBeDeleted.Id);
+            var id = teamService.AddTeamMember(newMemberName);
+            teamService.DeleteTeamMember(id);
+            //Assert
+            var member = teamService.GetTeamInfo().TeamMembers.Find(element => element.Name == "Boris");
+            Assert.Null(member);
+        }
+
+        [Fact]
+        public void TestGetAgeEqual()
+        {
+            // Assume
+            TeamMember teamMember = new TeamMember("Ioan");
+            teamMember.BirthDate = new DateTime(2000, 01, 01);
+            int expectedAge = 21;
+
+            // Act
+            int computedAge = teamMember.getAge();
+
+            // Assert
+            Assert.Equal(expectedAge, computedAge);
+        }
+
+        [Fact]
+        public void TestGetAgeNotEqual()
+        {
+            // Assume
+            TeamMember teamMember = new TeamMember("Ioan");
+            teamMember.BirthDate = new DateTime(2000, 01, 01);
+            int expectedAge = 1;
+
+            // Act
+            int computedAge = teamMember.getAge();
+
+            // Assert
+            Assert.NotEqual(expectedAge, computedAge);
+        }
+
+
+    }
+
+
+}
