bac76ef591d0f22f6042dc1ff1a9d47f51bcd661
parpa <parpa@DESKTOP-1JUTT8T>
2021-08-12 11:54:33 +0300

    TestCheckingConversion

--- HelloWorldWeb/Controllers/WeatherController.cs
+++ HelloWorldWeb/Controllers/WeatherController.cs
@@ -24,15 +24,17 @@ namespace HelloWorldWeb.Controllers
         {
             //lat 46.7700 lon 23.5800
             //https://api.openweathermap.org/data/2.5/onecall?lat=46.7700&lon=23.5800&exclude=hourly,minutely&appid=f0d03bf2ed843808710bf00ed7a9ad6f
+            
+            
             var client = new RestClient($"https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude=hourly,minutely&appid={apiKey}");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
             IRestResponse response = client.Execute(request);
-            return ConvertResponseToWeatherForecastList(response.Content);
+            return ConvertResponseToWeatherRecordList(response.Content);
            
         }
 
-        private IEnumerable<DailyWeatherRecord> ConvertResponseToWeatherForecastList(string content)
+        public IEnumerable<DailyWeatherRecord> ConvertResponseToWeatherRecordList(string content)
         {
             return new DailyWeatherRecord[] {
             new DailyWeatherRecord(new DateTime(2021,08,12), 22.0F, WeatherType.Mild),
