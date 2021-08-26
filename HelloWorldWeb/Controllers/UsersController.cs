e651bad955cd0b1b64ec18ea74e8e3ab716fb81b
parpa <parpa@DESKTOP-1JUTT8T>
2021-08-26 22:08:58 +0300

    Role manager added and working

--- HelloWorldWeb/Controllers/UsersController.cs
+++ HelloWorldWeb/Controllers/UsersController.cs
@@ -15,21 +15,40 @@ namespace HelloWorldWeb.Controllers
     {
 
         private readonly UserManager<IdentityUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
 
-        public UsersController(UserManager<IdentityUser> userManager)
+        public UsersController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             this.userManager = userManager;
+            this.roleManager = roleManager;
         }
 
         // GET: Users
         public async Task<IActionResult> Index()
         {
-            return View(await userManager.Users.ToListAsync());
+            List<UserWithRole> userWithRoles = new List<UserWithRole>();
+            var allUsers = await userManager.Users.ToListAsync();
+
+            var administrators = await userManager.GetUsersInRoleAsync("Administrators");
+            var commonUsers = allUsers.Except(administrators).ToList();
+            foreach (var admin in administrators)
+            {
+                UserWithRole dummy = new UserWithRole() { UserId = admin.Id, UserName = admin.UserName, RoleName = "Administrators" };
+                userWithRoles.Add(dummy);
+            }
+            foreach (var user in commonUsers)
+            {
+                UserWithRole dummy = new UserWithRole() { UserId = user.Id, UserName = user.UserName, RoleName = "Users" };
+                userWithRoles.Add(dummy);
+            }
+
+            return this.View(userWithRoles);
         }
 
         public async Task<IActionResult> AssignAdminRole(string id)
         {
             var user = await userManager.FindByIdAsync(id);
+            await this.userManager.RemoveFromRoleAsync(user, "Users");
             await userManager.AddToRoleAsync(user, "Administrators");
             return RedirectToAction(nameof(Index));
 
@@ -39,6 +58,7 @@ namespace HelloWorldWeb.Controllers
         {
             var user = await userManager.FindByIdAsync(id);
             await userManager.RemoveFromRoleAsync(user, "Administrators");
+            await this.userManager.AddToRoleAsync(user, "Users");
             return RedirectToAction(nameof(Index));
 
 
