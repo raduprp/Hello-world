1c793c61845833736a97c54955d89587087de01b
parpa <parpa@DESKTOP-1JUTT8T>
2021-08-26 11:22:15 +0300

    Administrator role created

--- HelloWorldWeb/Controllers/RolesController.cs
+++ HelloWorldWeb/Controllers/RolesController.cs
@@ -23,25 +23,22 @@ namespace HelloWorldWeb.Controllers
             return View(roleManager.Roles);
         }
 
-        // GET: RolesController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+       
 
         // GET: RolesController/Create
         public ActionResult Create()
         {
-            return View();
+            return View(new IdentityRole());
         }
 
         // POST: RolesController/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        
+        public async Task<ActionResult> Create(IdentityRole role)
         {
             try
             {
+                await roleManager.CreateAsync(role);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -56,40 +53,6 @@ namespace HelloWorldWeb.Controllers
             return View();
         }
 
-        // POST: RolesController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: RolesController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: RolesController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        
     }
 }
